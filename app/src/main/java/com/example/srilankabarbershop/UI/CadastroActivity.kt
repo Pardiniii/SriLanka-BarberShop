54485483083a65e4c181ee41c3a74a6fb4996e4d
Gustavo Pardini <ggpardini@gmail.com>
2025-04-01 23:49:03 -0300

    Iniciando projeto, criando primeiras telas

---
+++ app/src/main/java/com/example/srilankabarbershop/UI/CadastroActivity.kt
@@ -0,0 +1,19 @@
+package com.example.srilankabarbershop.UI
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.srilankabarbershop.R
+
+class CadastroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_cadastro)
+        supportActionBar?.hide()
+
+
+    }
+}
---
+++ app/src/main/java/com/example/srilankabarbershop/UI/NovaSenhaActivity.kt
@@ -0,0 +1,17 @@
+package com.example.srilankabarbershop.UI
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.srilankabarbershop.R
+
+class NovaSenhaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_nova_senha)
+
+    }
+}
