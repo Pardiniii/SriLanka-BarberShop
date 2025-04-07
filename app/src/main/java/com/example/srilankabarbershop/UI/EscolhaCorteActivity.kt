0e295f61dcd48ae2b1c323f0e76f47c107c7840e
Pardiniii <ggpardini@gmail.com>
2025-04-07 13:53:36 -0300

    ajustes na tela escolha corte

--- app/src/main/java/com/example/srilankabarbershop/UI/EscolhaCorteActivity.kt
+++ app/src/main/java/com/example/srilankabarbershop/UI/EscolhaCorteActivity.kt
@@ -1,16 +1,24 @@
 package com.example.srilankabarbershop.UI
 
+import android.graphics.Typeface
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.srilankabarbershop.R
+import com.example.srilankabarbershop.databinding.ActivityEscolhaCorteBinding
 
 class EscolhaCorteActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityEscolhaCorteBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_escolha_corte)
+        binding = ActivityEscolhaCorteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
 
     }
 }
