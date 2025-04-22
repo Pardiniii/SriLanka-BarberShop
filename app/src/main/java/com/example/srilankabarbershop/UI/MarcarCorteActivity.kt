a027d195203416120f4310514a39da0f2fe4aa27
Pardiniii <ggpardini@gmail.com>
2025-04-22 10:36:19 -0300

    Iniciando tela de calendario

---
+++ app/src/main/java/com/example/srilankabarbershop/UI/MarcarCorteActivity.kt
@@ -0,0 +1,18 @@
+package com.example.srilankabarbershop.UI
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.srilankabarbershop.R
+
+class MarcarCorteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_marcar_corte)
+
+
+    }
+}
